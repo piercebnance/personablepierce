835fac26c24829e07f8f98fd35bce3d5a7c2fe41
Pierce Nance <piercebnance@gmail.com>
2026-09-11 00:54:09 -0700

    preparing posts for project cards

--- piercedev/main.py
+++ piercedev/main.py
@@ -9,21 +9,22 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
 
-posts: list[dict] = [
+project_posts: list[dict] = [
     {
         "id": 1,
-        "author": "Corey Schafer",
-        "title": "FastAPI is Awesome",
-        "content": "This framework is really easy to use and super fast.",
-        "date_posted": "April 20, 2025",
+        "title": "Marketing Research with AI",
+        "content": "An AI powered webapp that can research marketing tactics and give you a summary of the results as well as a plan to implement.",
     },
     {
         "id": 2,
-        "author": "Jane Doe",
-        "title": "Python is Great for Web Development",
-        "content": "Python is a great language for web development, and FastAPI makes it even better.",
-        "date_posted": "April 21, 2025",
+        "title": "Amazon Web Scraper",
+        "content": "A web scraper built with Python, BeautifulSoup, and ScrapingBee to extract products with discounts from Amazon.",
     },
+    {
+        "id": 3,
+        "title": "Cleanup of a Large Dataset",
+        "content": "Intro project where I cleaned up and optimized a large dataset of 2400+ entries, removing duplicates, null values, and irrelevant data. I also performed data normalization and standardization to prepare the dataset for analysis.",
+    }
 ]
 
 
@@ -33,10 +34,10 @@ def home(request: Request):
     return templates.TemplateResponse(
         request,
         "home.html",
-        {"posts": posts, "title": "Home"},
+        {"posts": project_posts, "title": "Home"},
     )
 
 
 @app.get("/api/posts")
 def get_posts():
-    return posts
+    return project_posts
